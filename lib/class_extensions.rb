b7a478cfb7be5f45dfe230c8608adcbb84fc5dc6
Cassio Paes-Leme <cassio.paesleme@gmail.com>
2009-12-18 15:26:07 -0800

    added gruff and class_extensions file.

---
+++ lib/class_extensions.rb
@@ -0,0 +1,21 @@
+class Fixnum
+  def ch
+    return Chart.find(self)
+  end
+
+  def chs
+    return ChartSeries.find(self)
+  end
+
+  def s
+    return Series.find(self)
+  end
+
+  def sdp
+    return SeriesDataPoint.find(self)
+  end
+
+  def chl
+    return ChartLabel.find(self)
+  end
+end
