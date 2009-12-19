d06bd6cf689e952ff5ee9aa8bb7579a8251a12c7
Cassio Paes-Leme <cassio.paesleme@gmail.com>
2009-12-18 16:04:12 -0800

    switched to charts controller

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -6,24 +6,17 @@ class Chart < ActiveRecord::Base
   def create_file
     g=Gruff::Bar.new
 
-    if self.series.length==1 then
-      s = self.series[0]
+    self.series.each do |s|
       series_name = s.name
       series_data_array = s.series_data_points.collect{|d| d.value}
       g.data(series_name,series_data_array )
-    else
-      self.series.each do |s|
-        series_name = s.name
-        series_data_array = s.series_data_points.collect{|d| d.value}
-        g.data(series_name,series_data_array )
-      end
     end
 
     self.attributes.select{|k,v| v.class != Time && k!='id'}.each do |k,v|
       g.send(k.to_s + '=',v) if v
     end
     
-    new_file_name = 'test.png'
+    new_file_name = 'public/images/test.png'
 
     g.write(new_file_name)
 
