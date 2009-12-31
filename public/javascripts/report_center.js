fc0b02eb3ea1f1e04347f556cdfb15d47d24b549
unknown <cpaesleme@.(none)>
2009-12-31 10:51:07 -0800

    stylistic changes, amended spinners to appear before ajax call

--- public/javascripts/report_center.js
+++ public/javascripts/report_center.js
@@ -1,7 +1,6 @@
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+this makes sure ajax calls to check for files don't go off their caches */
+$.ajaxSettings.cache = false; 
 function getChildren(id,parent_name,new_s_name,new_c_name){
     //submit request to server
 
@@ -10,7 +9,8 @@ function getChildren(id,parent_name,new_s_name,new_c_name){
     var query_name = child_title_panel.find(".chart_name").attr('query_name');
     var series_panel=child_title_panel.find(".series_name")
     var curr_s_name=series_panel.attr('name');
-    series_panel.text(new_s_name.toProperCase());
+    //add comma delimiter to series_panel
+    series_panel.text(new_s_name.toProperCase()+',');
     series_panel.attr('name',new_s_name)
     var category_panel=child_title_panel.find(".category_name")
     var curr_c_name=category_panel.attr('name');
@@ -20,6 +20,13 @@ function getChildren(id,parent_name,new_s_name,new_c_name){
     var rank = 0;
     //use parent name to cycle through child embeds
     var children = $("embed:[id^=" +parent_name + "_child]")
+    //hide all charts while we get their data
+    children.each(function(){
+        var chObj= getChartFromId(this.id);
+        $(chObj).parent().hide();
+        //show spinner
+        $(chObj).parent().parent().find('div.spinner').show();
+    })
     children.each(function(){
         rank+=1;
         //replace chart data URL with itself modified for new series and category names
@@ -42,8 +49,8 @@ function getChildren(id,parent_name,new_s_name,new_c_name){
         $.ajax({
             url:file_url,
             query_string: 'series_name=' + new_s_name +
-                  '&category_name=' + new_c_name +
-                  '&rank=' + rank,
+            '&category_name=' + new_c_name +
+            '&rank=' + rank,
             new_fv:new_fv,
             src:src,
             chObj: chObj,
@@ -51,11 +58,10 @@ function getChildren(id,parent_name,new_s_name,new_c_name){
             success: function(){
                 $(this.chObj).attr('flashvars',new_fv);
                 this.chObj.src=src;
+                $(this.chObj).parent().parent().find('div.spinner').hide();
+                $(this.chObj).parent().show();
             },
             error: function(){
-                $(this.chObj).parent().hide();
-                //show spinner
-                $(this.chObj).parent().parent().find('div.spinner').show();
                 $.ajax({
                     data: this.query_string,
                     dataType:'script',
@@ -67,7 +73,8 @@ function getChildren(id,parent_name,new_s_name,new_c_name){
                         $(this.chObj).attr('flashvars',new_fv);
                         this.chObj.src=this.src;
                         $(this.chObj).parent().parent().find('div.spinner').hide();
-                        $(this.chObj).parent().show()},
+                        $(this.chObj).parent().show();
+                        },
                     error:function(){
                         alert('Ajax error - Please check your internet connection.')
                     }
